08183d540de2bb6ec2019f5248bbf5558ea32543
H82M19\Student <Student>
2024-09-07 19:45:01 +0700

    first commit

---
+++ src/main/java/com/example/week01_lab_nguyenthientu_21111951/controllers/ControllerServlet.java
@@ -0,0 +1,9 @@
+package com.example.week01_lab_nguyenthientu_21111951.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name ="controller", value ="/controller" )
+public class ControllerServlet extends HttpServlet {
+
+}
---
+++ src/main/java/com/example/week01_lab_nguyenthientu_21111951/services/AccountService.java
@@ -0,0 +1,4 @@
+package com.example.week01_lab_nguyenthientu_21111951.services;
+
+public class AccountService {
+}
